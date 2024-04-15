b83e97e2a50f1b0597c4ebcd4a80c90205fd0827
amina <amina.1111@yahoo.fr>
2024-04-15 16:00:28 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,40 +8,49 @@ returns information about his/her todo list progress.
 import requests
 import sys
 
-base_url = "https://jsonplaceholder.typicode.com/"  # the API endpoint
-response_users = requests.get(f"{base_url}/users")  # A GET request to the API
-response_users_json = response_users.json()  # JSON decoding
-listofids = []
-for index in range(len(response_users_json)):
-    for key in response_users_json[index]:
-        if key == "id":
-            listofids.append(
-                response_users_json[index]["id"]
-            )  # creating a list of user IDs
 
-arg1 = int(sys.argv[1])  # the first parameter (user id)
-if arg1 in listofids:
-    TOTAL_NUMBER_OF_TASKS = 0  # total number of tasks initialization
-    NUMBER_OF_DONE_TASKS = 0  # number of done tasks initialization
-    response_todos = requests.get(f"{base_url}/todos")
-    response_todos_json = response_todos.json()
+def Gather_data(arg1):
+    base_url = "https://jsonplaceholder.typicode.com/"  # the API endpoint
+    response_users = requests.get(
+        f"{base_url}/users"
+        )  # A GET request to the API
+    response_users_json = response_users.json()  # JSON decoding
+    listofids = []
     for index in range(len(response_users_json)):
-        if response_users_json[index]["id"] == arg1:
-            EMPLOYEE_NAME = response_users_json[index][
-                "name"
-            ]  # fetching employee names
+        for key in response_users_json[index]:
+            if key == "id":
+                listofids.append(
+                    response_users_json[index]["id"]
+                )  # creating a list of user IDs
 
-    for index in range(len(response_todos_json)):
-        if response_todos_json[index]["userId"] == arg1:
-            TOTAL_NUMBER_OF_TASKS += 1
-            if response_todos_json[index]["completed"] is True:
-                NUMBER_OF_DONE_TASKS += 1
-    print(
-        f"Employee {EMPLOYEE_NAME} is \
+    if arg1 in listofids:  # check if the first parameter is a user id
+        TOTAL_NUMBER_OF_TASKS = 0  # total number of tasks initialization
+        NUMBER_OF_DONE_TASKS = 0  # number of done tasks initialization
+        response_todos = requests.get(f"{base_url}/todos")
+        response_todos_json = response_todos.json()
+        for index in range(len(response_users_json)):
+            if response_users_json[index]["id"] == arg1:
+                EMPLOYEE_NAME = response_users_json[index][
+                    "name"
+                ]  # fetching employee names
+
+        for index in range(len(response_todos_json)):
+            if response_todos_json[index]["userId"] == arg1:
+                TOTAL_NUMBER_OF_TASKS += 1
+                if response_todos_json[index]["completed"] is True:
+                    NUMBER_OF_DONE_TASKS += 1
+        print(
+            f"Employee {EMPLOYEE_NAME} is \
 done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
-    for index in range(len(response_todos_json)):
-        if response_todos_json[index]["userId"] == arg1:
-            if response_todos_json[index]["completed"] is True:
-                TASK_TITLE = f"\t {response_todos_json[index]['title']}"
-                print(TASK_TITLE)  # printing list of done tasks
+        for index in range(len(response_todos_json)):
+            if response_todos_json[index]["userId"] == arg1:
+                if response_todos_json[index]["completed"] is True:
+                    TASK_TITLE = f"\t {response_todos_json[index]['title']}"
+                    print(TASK_TITLE)  # printing list of done tasks
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        arg1 = int(sys.argv[1])  # the first parameter (user id)
+        Gather_data(arg1)
