a72f2cec5454d2538f0229b4a10f208aba3b37c6
amina <amina.1111@yahoo.fr>
2024-04-15 17:12:01 +0100

    exporting data in the csv format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -30,20 +30,12 @@ def export_to_csv(USER_ID):
                 # fetching employee names and usernames
 
         with open(f"{USER_ID}.csv", "w", newline="") as file:
-            csvdata = csv.writer(file)  # exporting data in the csv format
 
             for index in range(len(response_todos_json)):
                 if response_todos_json[index]["userId"] == USER_ID:
                     TASK_COMPLETED_STATUS = response_todos_json[index]["completed"]
                     TASK_TITLE = response_todos_json[index]["title"]
-                    csvdata.writerow(
-                        [
-                            '\"{}\"'.format(USER_ID),
-                            '\"{}\"'.format(USERNAME),
-                            '\"{}\"'.format(TASK_COMPLETED_STATUS),
-                            '\"{}\"'.format(TASK_TITLE)
-                        ]
-                    )  # writing rows
+                    file.write(f'"{USER_ID}","{USERNAME}","{TASK_COMPLETED_STATUS}","{TASK_TITLE}"\n')  # writing rows
         file.close()
 
 
